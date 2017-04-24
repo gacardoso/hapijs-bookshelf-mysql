83e2ec35d1092fd5328cb3c376dd6a205c7867fd
“gacardoso” <gcardosoti@gmail.com>
2017-04-23 22:40:22 -0300

    validate password

--- index.js
+++ index.js
@@ -13,11 +13,30 @@ server.route({
     handler: function (request, reply) {
         console.log(request.payload);
         User.forge(request.payload)
-        .save()
-        .then((user) => reply(user), (err) => reply(err) )
+            .save()
+            .then((user) => reply(user), (err) => reply(err))
     },
-    config:{
-        validate:{
+    config: {
+        validate: {
+            payload: Joi.object({
+                email: Joi.string().email().required(),
+                password: Joi.string().required(),
+            })
+        }
+    }
+});
+
+server.route({
+    method: 'POST',
+    path: '/v1/sessions',
+    handler: function (request, reply) {
+        User.forge({ email: request.payload.email })
+            .fetch({ require: true })
+            .then((user) => user.compare(request.payload.password))
+            .then((isValid) => reply({login: isValid}))
+    },
+    config: {
+        validate: {
             payload: Joi.object({
                 email: Joi.string().email().required(),
                 password: Joi.string().required(),
