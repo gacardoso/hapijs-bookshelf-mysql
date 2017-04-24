a125aae2cb81dae092a36009c2bdfe7cbeeaf113
“gacardoso” <gcardosoti@gmail.com>
2017-04-24 00:12:09 -0300

    jwt

--- index.js
+++ index.js
@@ -3,9 +3,13 @@
 let Joi = require('joi');
 let Hapi = require('hapi');
 let User = require('./models/user')
-let server = new Hapi.Server();
+let jwt = require('jsonwebtoken');
+let Boom = require('boom');
 
+let server = new Hapi.Server();
 server.connection({ port: 3000, host: 'localhost' });
+
+const JWT_KEY = 'here be dragons';
 
 server.route({
     method: 'POST',
@@ -30,10 +34,25 @@ server.route({
     method: 'POST',
     path: '/v1/sessions',
     handler: function (request, reply) {
+
+        let user;
+
         User.forge({ email: request.payload.email })
             .fetch({ require: true })
-            .then((user) => user.compare(request.payload.password))
-            .then((isValid) => reply({login: isValid}))
+            .then((result) => {
+                user = result
+                return result.compare(request.payload.password)
+            })
+            .then((isValid) => {
+
+                if (isValid) {
+                    reply({
+                        token: jwt.sign({ id: user.id }, JWT_KEY)
+                    })
+                } else {
+                    reply(Boom.unauthorized())
+                }
+            })
     },
     config: {
         validate: {
